3ab88a2e9f4abaf443e08111a1e9a1ca283d82d0
Neeraj Kumar <aaditya.neer@gmail.com>
2025-06-10 10:05:07 +0530

    first commit

---
+++ src/main/java/com/example/hello/VariableExample.java
@@ -0,0 +1,21 @@
+package com.example.hello;
+
+public class VariableExample {
+    public static void main(String[] args) {
+        // Declare and initialize a variable
+        int number = 10;
+
+        // Print the variable
+        System.out.println("The value of the variable is: " + number);
+
+        // Change the value of the variable
+        number = 20;
+
+        // Print the updated variable
+        System.out.println("The updated value of the variable is: " + number);
+    }
+    //write a method to sum two numbers
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
